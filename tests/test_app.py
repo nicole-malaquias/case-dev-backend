acc0b6c4785bc9b6401bfc1c2a4457da23c09830
Nicole Malaquias <nicolemalaquias91@gmail.com>
2024-01-29 12:46:48 -0300

    feat(tournament): implementing test configuration

---
+++ tests/test_app.py
@@ -0,0 +1,32 @@
+from fastapi.testclient import TestClient
+
+from app.app import app
+
+
+def test_create_tournament_success():
+    client = TestClient(app)
+    payload = {
+        'name': 'Torneio de Exemplo',
+        'date_start': '2024-01-29T12:00:00',
+        'date_end': '2024-02-05T18:00:00',
+    }
+    response = client.post('/tournament', json=payload)
+
+    assert response.status_code == 201
+    assert response.json() == {
+        'name': 'Torneio de Exemplo',
+        'date_start': '2024-01-29T12:00:00',
+        'date_end': '2024-02-05T18:00:00',
+    }
+
+
+def test_create_tournament_failure():
+    client = TestClient(app)
+    payload = {
+        'name': 'Torneio de Exemplo',
+        'date_start': '2024-02-05T18:00:00',
+        'date_end': '2024-01-29T12:00:00',
+    }
+    response = client.post('/tournament', json=payload)
+
+    assert response.status_code == 422
