923396ce9b7c1ab5e5218cbdf10d60d59ce2530e
Nicole Malaquias <nicolemalaquias91@gmail.com>
2024-01-29 12:43:37 -0300

    feat(tournament):created schema and model for tournament

--- app/app.py
+++ app/app.py
@@ -1,18 +1,7 @@
 from fastapi import FastAPI
 
-from .schemas import TournamentSchema
+from app.routes import routes
 
 app = FastAPI()
 
-
-@app.post('/tournament/', status_code=201, response_model=TournamentSchema)
-def create_tournament(tournament: TournamentSchema):
-    """
-    Cria um torneio
-
-    - **name**: Nome do torneio
-    - **date_start**: Data de início do torneio
-    - **date_end**: Data de término do torneio
-
-    """
-    return tournament
+app.include_router(routes.router)
---
+++ app/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Tournament(Base):
+    __tablename__ = 'tournaments'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    date_start = Column(DateTime, nullable=False)
+    date_end = Column(DateTime, nullable=False)
