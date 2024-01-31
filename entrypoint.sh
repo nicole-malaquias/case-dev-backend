701b8339a127a343b0568b61ae3ac1ddf81c6340
Nicole Malaquias <nicolemalaquias91@gmail.com>
2024-01-30 21:14:37 -0300

    feat(competitor): Some adjustments to the files to run migrate when the container starts.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-poetry run alembic upgrade head
+# Adicione um pequeno atraso para garantir que o banco de dados esteja pronto
+sleep 10
 
+poetry run alembic upgrade head
 poetry run uvicorn --host 0.0.0.0 --port 8000 app.app:app
