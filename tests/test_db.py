1a540676e944f43b13c979a321cfe36ad6780444
Nicole Malaquias <nicolemalaquias91@gmail.com>
2024-02-01 14:15:50 -0300

    feat(add-winner):Made adjustments to the tests and adding tests.

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,14 +1,17 @@
+import logging
 from datetime import datetime
 
 import pytest
-from sqlalchemy import select
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import select
 
 from app.models import Competitor, Tournament
 
+logger = logging.getLogger(__name__)
+
 
 def test_create_tournament(session):
-    # Crie um novo torneio
+    logger.info('Testing creating a tournament...')
     new_tournament = Tournament(
         name='Sample Tournament',
         date_start=datetime.now(),
@@ -17,34 +20,32 @@ def test_create_tournament(session):
     session.add(new_tournament)
     session.commit()
 
-    # Consulte o torneio pelo nome
     tournament = session.scalar(
         select(Tournament).where(Tournament.name == 'Sample Tournament')
     )
 
-    # Verifique se o torneio foi criado corretamente
     assert tournament.name == 'Sample Tournament'
     assert tournament.date_start is not None
     assert tournament.date_end is not None
+    logger.info('Tournament created successfully.')
 
 
 def test_create_competitor_invalid_tournament_id(session: Session):
-    # Tente criar competidores com um 'tournament_id' que não existe
-    invalid_tournament_id = 999  # Um ID que não deve existir no banco de dados
+    logger.info('Testing creating a competitor with invalid tournament ID...')
+    invalid_tournament_id = 999
 
-    # Dados do competidor a serem criados
     competitor_names = ['Competitor1', 'Competitor2']
 
-    # Tente criar competidores com um 'tournament_id' inválido
     with pytest.raises(ValueError) as exc_info:
         Competitor.create_competitor(
             competitor_names, invalid_tournament_id, session
         )
 
-    # Verifique se a exceção tem a mensagem esperada
     assert 'Tournament with ID' in str(exc_info.value)
     assert str(invalid_tournament_id) in str(exc_info.value)
 
-    # Certifique-se de que não há competidores criados no banco de dados
     competitors = session.query(Competitor).all()
     assert len(competitors) == 0
+    logger.info(
+        'Competitor creation with invalid tournament ID tested successfully.'
+    )
